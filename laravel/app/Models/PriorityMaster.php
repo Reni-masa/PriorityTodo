0d22e844d1d1c623c3ceadc978341bb0e73924d2
SugitaMasataka <mstk.ambit@gmail.com>
2021-12-13 14:58:35 +0900

    PriorityMaster Todo リレーション作成

---
+++ laravel/app/Models/PriorityMaster.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PriorityMaster extends Model
+{
+    use HasFactory;
+
+    protected $table = 'priority_masters';
+
+    protected $fillable = ['name'];
+
+    /**
+     * PriorityMasterのtodoを取得
+     */
+    public function todos()
+    {
+        return $this->hasMany(Todo::class,'priority_id');
+    }
+}
---
+++ laravel/app/Models/Todo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'todos';
+
+    protected $fillable = ['content','deadline_date','priority_id'];
+
+    /**
+     * Todoに紐づくPriorityMasterを取得
+     */
+    public function priority_master()
+    {
+        return $this->belongsTo(PriorityMaster::class,'priority_id');
+    }
+}
